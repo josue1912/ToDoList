db7f011007d2856dcc4620607774886c2684ff31
Josue Freitas <josue1912@gmail.com>
2018-01-06 23:44:31 -0200

    Criando o contexto de banco de dados

---
+++ Models/TodoContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItem{get;set;}
+    }
+}
